abc75e96a8bb9526a596eb584fbd159d30da72c5
Tharusha Lakshan <ktharushalakshan123@gmail.com>
2026-06-01 15:47:56 +0530

    Add dashboard assets and constants

---
+++ lib/core/utils/app_strings.dart
@@ -0,0 +1,15 @@
+class AppStrings {
+  static const userName = 'Jenna';
+
+  static const dashboardGreeting =
+      'Let\'s start exercising';
+
+  static const recommended =
+      'Recommended for you';
+
+  static const trainerTitle =
+      'Find me a personal Trainer';
+
+  static const groupTitle =
+      'Find me group classes';
+}
